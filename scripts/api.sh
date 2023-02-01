c5afb4101a2f7c666a29e54694cf6b1639295f6b
David Montoya <davidmontoyago@gmail.com>
2023-01-31 20:37:12 -0600

    parse quote body and author

--- scripts/api.sh
+++ scripts/api.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CURRENT_DIR/helpers.sh"
+
 get_meditation() {
     meditation="$(curl https://stoicquotesapi.com/v1/api/quotes/random)"
-    echo ${meditation}
+
+    quote="$(json_extract "body" "${meditation}")"
+    quote="${quote%\"}"
+    quote="${quote#\"}"
+
+    author="$(json_extract "author" "${meditation}")"
+    author="${author%\"}"
+    author="${author#\"}"
+
+    echo "${quote} --${author}"
 }
 
 main() {
