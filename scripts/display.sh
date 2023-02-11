23eb7ec16e2ca8585be396254e3a7b9787945bf4
David Montoya <davidmontoyago@gmail.com>
2023-02-10 22:38:21 -0600

    display accept button

--- scripts/display.sh
+++ scripts/display.sh
@@ -6,14 +6,21 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # tmux display-message "Meditation"
 
 main() {
-    meditation="$($CURRENT_DIR/api.sh)"
-    # display in the current pane
-    # echo ${meditation}
+    declare meditation="$($CURRENT_DIR/api.sh)"
+
+    declare -i padding=(55-15)/2
 
     # display as popup
-    export meditation="$meditation" &&
-        tmux display-popup -E -b "double" -h15 -w55 "printf '\n%s\n\n\n' \"${meditation}\" & \
-            while true ; do read -t 30 -n 1 ; if [ $? = 0 ] ; then exit 0 ; fi ; done"
+    export meditation="$meditation" padding=$padding &&
+        tmux display-popup -E \
+            -b "single" -S "fg=green" -h15 -w55 \
+            "printf '\n%s\n\n\n\n\n\n%c\e[%db\e[30m\e[102m%s' \"${meditation}\" \" \" $padding \"<Accept>\" & \
+            while true ; \
+            do read -t 30 -n 1 ; \
+            if [ $? = 0 ] ; then \
+                exit 0 ; \
+            fi ; \
+            done"
 }
 
 main
