65806454312ddce1e91666a8dde154ccf7d97125
David Montoya <davidmontoyago@gmail.com>
2023-02-12 15:51:37 -0600

    tab author

--- scripts/api/v2.sh
+++ scripts/api/v2.sh
@@ -17,7 +17,7 @@ get_quote() {
     author="${author%\"}"
     author="${author#\"}"
 
-    printf ' %s\n\n%s' "${quote}" "${author}" | fold -s -w 50 | sed -e "2,\$s/^/ /"
+    printf ' %s\n\n\t\t\t\t%s' "${quote}" "${author}" | fold -s -w 50 | sed -e "2,\$s/^/ /"
 }
 
 main() {
